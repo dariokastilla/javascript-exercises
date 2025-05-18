057d1f7c921eb96a51e76d467b374e7311d85173
dariokastilla <dariokastillam@gmail.com>
2025-05-18 13:01:42 +0200

    Objects exercises done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(books) {
+      
+      const titles = books.map(function(book){
+          return book.title;
+      })
+      
+      
+      return titles;
+  };
 
 // Do not edit below this line
 module.exports = getTheTitles;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,46 @@
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+  
+  
+  
+  
 const findTheOldest = function() {
-
+  
+  const oldest = people.sort(function(a, b){
+      let lastPerson = a.yearOfDeath - a.yearOfBirth;
+      let nextPerson = b.yearOfDeath - b.yearOfBirth;
+      
+      if(lastPerson > nextPerson){
+          return -1;
+      }
+      else if(lastPerson < nextPerson){
+          return 1;
+      }
+      else{
+          return 0;
+      }
+      
+  })
+  return oldest[0];
+  
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
